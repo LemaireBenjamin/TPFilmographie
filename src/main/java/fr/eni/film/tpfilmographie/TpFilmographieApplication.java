9d141ce424d809c8b4d97beba7ff931fba88067a
Benjamin LEMAIRE <benjamin.lemaire2023@campus-eni.fr>
2023-08-09 16:35:15 +0200

    Init project

---
+++ src/main/java/fr/eni/film/tpfilmographie/TpFilmographieApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.film.tpfilmographie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFilmographieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpFilmographieApplication.class, args);
+    }
+
+}
