251e55cc4615381cf9b828364562f14a7dc80bc5
Flegoff2318 <96115160+Flegoff2318@users.noreply.github.com>
2023-08-10 14:08:17 +0200

    constructors & getters/setters on BO's

--- src/main/java/fr/eni/film/tpfilmographie/bo/Movie.java
+++ src/main/java/fr/eni/film/tpfilmographie/bo/Movie.java
@@ -3,6 +3,7 @@ package fr.eni.film.tpfilmographie.bo;
 import java.util.ArrayList;
 
 public class Movie {
+    private int id;
     private String title;
     private int releaseDate;
     private int duration;
@@ -11,4 +12,91 @@ public class Movie {
     private ArrayList<Opinion> opinions;
     private ArrayList<Participant> actors;
     private Participant director;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title, int releaseDate, int duration, String synopsis, Type type, ArrayList<Opinion> opinions, ArrayList<Participant> actors, Participant director) {
+        this.id = id;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+        this.synopsis = synopsis;
+        this.type = type;
+        this.opinions = opinions;
+        this.actors = actors;
+        this.director = director;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public ArrayList<Opinion> getOpinions() {
+        return opinions;
+    }
+
+    public void setOpinions(ArrayList<Opinion> opinions) {
+        this.opinions = opinions;
+    }
+
+    public ArrayList<Participant> getActors() {
+        return actors;
+    }
+
+    public void setActors(ArrayList<Participant> actors) {
+        this.actors = actors;
+    }
+
+    public Participant getDirector() {
+        return director;
+    }
+
+    public void setDirector(Participant director) {
+        this.director = director;
+    }
 }
--- src/main/java/fr/eni/film/tpfilmographie/bo/Opinion.java
+++ src/main/java/fr/eni/film/tpfilmographie/bo/Opinion.java
@@ -1,7 +1,50 @@
 package fr.eni.film.tpfilmographie.bo;
 
 public class Opinion {
+    private int id;
     private String opinion;
     private byte rating;
     private Movie movie;
+
+    public Opinion(int id, String opinion, byte rating, Movie movie) {
+        this.id = id;
+        this.opinion = opinion;
+        this.rating = rating;
+        this.movie = movie;
+    }
+
+    public Opinion() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOpinion() {
+        return opinion;
+    }
+
+    public void setOpinion(String opinion) {
+        this.opinion = opinion;
+    }
+
+    public byte getRating() {
+        return rating;
+    }
+
+    public void setRating(byte rating) {
+        this.rating = rating;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
 }
